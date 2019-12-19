af1708fa39a98759540e885cb10951ecc263e8e9
jasonloardyy <43225118+jasonloardyy@users.noreply.github.com>
2019-12-19 09:14:37 +0800

    form peminjaman sls

--- perpustakaan/perpustakaan/FormPeminjaman.Designer.vb
+++ perpustakaan/perpustakaan/FormPeminjaman.Designer.vb
@@ -24,29 +24,29 @@ Partial Class FormPeminjaman
     Private Sub InitializeComponent()
         Me.tbkdanggota = New System.Windows.Forms.TextBox()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.tbnama = New System.Windows.Forms.TextBox()
-        Me.tbalamat = New System.Windows.Forms.TextBox()
-        Me.tbnotelp = New System.Windows.Forms.TextBox()
-        Me.tbrole = New System.Windows.Forms.TextBox()
-        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
-        Me.dgv = New System.Windows.Forms.DataGridView()
-        Me.tbkdbuku = New System.Windows.Forms.TextBox()
-        Me.tbjudulbuku = New System.Windows.Forms.TextBox()
-        Me.btnbuku = New System.Windows.Forms.Button()
-        Me.dtpkembalibuku = New System.Windows.Forms.DateTimePicker()
-        Me.dtppinjaman = New System.Windows.Forms.DateTimePicker()
-        Me.tbkdpeminjaman = New System.Windows.Forms.TextBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.btninput = New System.Windows.Forms.Button()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.Label7 = New System.Windows.Forms.Label()
-        Me.Label8 = New System.Windows.Forms.Label()
-        Me.Label9 = New System.Windows.Forms.Label()
         Me.btnanggota = New System.Windows.Forms.Button()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.tbkdpeminjaman = New System.Windows.Forms.TextBox()
+        Me.dtppinjaman = New System.Windows.Forms.DateTimePicker()
+        Me.tbrole = New System.Windows.Forms.TextBox()
+        Me.tbnotelp = New System.Windows.Forms.TextBox()
+        Me.tbalamat = New System.Windows.Forms.TextBox()
+        Me.tbnama = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btninput = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.dtpkembalibuku = New System.Windows.Forms.DateTimePicker()
+        Me.btnbuku = New System.Windows.Forms.Button()
+        Me.tbjudulbuku = New System.Windows.Forms.TextBox()
+        Me.tbkdbuku = New System.Windows.Forms.TextBox()
+        Me.dgv = New System.Windows.Forms.DataGridView()
         Me.btnsimpan = New System.Windows.Forms.Button()
         Me.btnbatal = New System.Windows.Forms.Button()
         Me.btnhpsitem = New System.Windows.Forms.Button()
@@ -87,21 +87,99 @@ Partial Class FormPeminjaman
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Transaksi Peminjaman"
         '
-        'tbnama
+        'btnanggota
         '
-        Me.tbnama.Enabled = False
-        Me.tbnama.Location = New System.Drawing.Point(445, 45)
-        Me.tbnama.Name = "tbnama"
-        Me.tbnama.Size = New System.Drawing.Size(181, 20)
-        Me.tbnama.TabIndex = 2
+        Me.btnanggota.Location = New System.Drawing.Point(594, 19)
+        Me.btnanggota.Name = "btnanggota"
+        Me.btnanggota.Size = New System.Drawing.Size(32, 20)
+        Me.btnanggota.TabIndex = 12
+        Me.btnanggota.Text = "..."
+        Me.btnanggota.UseVisualStyleBackColor = True
         '
-        'tbalamat
+        'Label9
         '
-        Me.tbalamat.Enabled = False
-        Me.tbalamat.Location = New System.Drawing.Point(445, 71)
-        Me.tbalamat.Name = "tbalamat"
-        Me.tbalamat.Size = New System.Drawing.Size(181, 20)
-        Me.tbalamat.TabIndex = 3
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(650, 74)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(29, 13)
+        Me.Label9.TabIndex = 18
+        Me.Label9.Text = "Role"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(650, 51)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(66, 13)
+        Me.Label8.TabIndex = 17
+        Me.Label8.Text = "No. Telepon"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(359, 74)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(39, 13)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "Alamat"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(359, 48)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(35, 13)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Nama"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(359, 22)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(66, 13)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Kd. Anggota"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 48)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(85, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Tgl. Peminjaman"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 22)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(83, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Kd. Peminjaman"
+        '
+        'tbkdpeminjaman
+        '
+        Me.tbkdpeminjaman.Location = New System.Drawing.Point(108, 19)
+        Me.tbkdpeminjaman.Name = "tbkdpeminjaman"
+        Me.tbkdpeminjaman.Size = New System.Drawing.Size(200, 20)
+        Me.tbkdpeminjaman.TabIndex = 10
+        '
+        'dtppinjaman
+        '
+        Me.dtppinjaman.Location = New System.Drawing.Point(108, 45)
+        Me.dtppinjaman.Name = "dtppinjaman"
+        Me.dtppinjaman.Size = New System.Drawing.Size(200, 20)
+        Me.dtppinjaman.TabIndex = 9
+        '
+        'tbrole
+        '
+        Me.tbrole.Enabled = False
+        Me.tbrole.Location = New System.Drawing.Point(722, 71)
+        Me.tbrole.Name = "tbrole"
+        Me.tbrole.Size = New System.Drawing.Size(100, 20)
+        Me.tbrole.TabIndex = 5
         '
         'tbnotelp
         '
@@ -111,13 +189,21 @@ Partial Class FormPeminjaman
         Me.tbnotelp.Size = New System.Drawing.Size(100, 20)
         Me.tbnotelp.TabIndex = 4
         '
-        'tbrole
+        'tbalamat
         '
-        Me.tbrole.Enabled = False
-        Me.tbrole.Location = New System.Drawing.Point(722, 71)
-        Me.tbrole.Name = "tbrole"
-        Me.tbrole.Size = New System.Drawing.Size(100, 20)
-        Me.tbrole.TabIndex = 5
+        Me.tbalamat.Enabled = False
+        Me.tbalamat.Location = New System.Drawing.Point(445, 71)
+        Me.tbalamat.Name = "tbalamat"
+        Me.tbalamat.Size = New System.Drawing.Size(181, 20)
+        Me.tbalamat.TabIndex = 3
+        '
+        'tbnama
+        '
+        Me.tbnama.Enabled = False
+        Me.tbnama.Location = New System.Drawing.Point(445, 45)
+        Me.tbnama.Name = "tbnama"
+        Me.tbnama.Size = New System.Drawing.Size(181, 20)
+        Me.tbnama.TabIndex = 2
         '
         'GroupBox2
         '
@@ -135,66 +221,14 @@ Partial Class FormPeminjaman
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Input Buku"
         '
-        'dgv
+        'btninput
         '
-        Me.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgv.Location = New System.Drawing.Point(12, 178)
-        Me.dgv.Name = "dgv"
-        Me.dgv.Size = New System.Drawing.Size(860, 218)
-        Me.dgv.TabIndex = 5
-        '
-        'tbkdbuku
-        '
-        Me.tbkdbuku.Location = New System.Drawing.Point(70, 19)
-        Me.tbkdbuku.Name = "tbkdbuku"
-        Me.tbkdbuku.Size = New System.Drawing.Size(100, 20)
-        Me.tbkdbuku.TabIndex = 6
-        '
-        'tbjudulbuku
-        '
-        Me.tbjudulbuku.Location = New System.Drawing.Point(214, 19)
-        Me.tbjudulbuku.Name = "tbjudulbuku"
-        Me.tbjudulbuku.Size = New System.Drawing.Size(272, 20)
-        Me.tbjudulbuku.TabIndex = 7
-        '
-        'btnbuku
-        '
-        Me.btnbuku.Location = New System.Drawing.Point(176, 19)
-        Me.btnbuku.Name = "btnbuku"
-        Me.btnbuku.Size = New System.Drawing.Size(32, 20)
-        Me.btnbuku.TabIndex = 6
-        Me.btnbuku.Text = "..."
-        Me.btnbuku.UseVisualStyleBackColor = True
-        '
-        'dtpkembalibuku
-        '
-        Me.dtpkembalibuku.Location = New System.Drawing.Point(566, 19)
-        Me.dtpkembalibuku.Name = "dtpkembalibuku"
-        Me.dtpkembalibuku.Size = New System.Drawing.Size(200, 20)
-        Me.dtpkembalibuku.TabIndex = 8
-        '
-        'dtppinjaman
-        '
-        Me.dtppinjaman.Location = New System.Drawing.Point(108, 45)
-        Me.dtppinjaman.Name = "dtppinjaman"
-        Me.dtppinjaman.Size = New System.Drawing.Size(200, 20)
-        Me.dtppinjaman.TabIndex = 9
-        '
-        'tbkdpeminjaman
-        '
-        Me.tbkdpeminjaman.Location = New System.Drawing.Point(108, 19)
-        Me.tbkdpeminjaman.Name = "tbkdpeminjaman"
-        Me.tbkdpeminjaman.Size = New System.Drawing.Size(200, 20)
-        Me.tbkdpeminjaman.TabIndex = 10
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(6, 22)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(51, 13)
-        Me.Label1.TabIndex = 9
-        Me.Label1.Text = "Kd. Buku"
+        Me.btninput.Location = New System.Drawing.Point(779, 16)
+        Me.btninput.Name = "btninput"
+        Me.btninput.Size = New System.Drawing.Size(75, 23)
+        Me.btninput.TabIndex = 11
+        Me.btninput.Text = "Input"
+        Me.btninput.UseVisualStyleBackColor = True
         '
         'Label2
         '
@@ -205,86 +239,52 @@ Partial Class FormPeminjaman
         Me.Label2.TabIndex = 10
         Me.Label2.Text = "Tgl. Kembali"
         '
-        'btninput
+        'Label1
         '
-        Me.btninput.Location = New System.Drawing.Point(779, 16)
-        Me.btninput.Name = "btninput"
-        Me.btninput.Size = New System.Drawing.Size(75, 23)
-        Me.btninput.TabIndex = 11
-        Me.btninput.Text = "Input"
-        Me.btninput.UseVisualStyleBackColor = True
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Kd. Buku"
         '
-        'Label3
+        'dtpkembalibuku
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(6, 22)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(83, 13)
-        Me.Label3.TabIndex = 12
-        Me.Label3.Text = "Kd. Peminjaman"
+        Me.dtpkembalibuku.Location = New System.Drawing.Point(566, 19)
+        Me.dtpkembalibuku.Name = "dtpkembalibuku"
+        Me.dtpkembalibuku.Size = New System.Drawing.Size(200, 20)
+        Me.dtpkembalibuku.TabIndex = 8
         '
-        'Label4
+        'btnbuku
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(6, 48)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(85, 13)
-        Me.Label4.TabIndex = 13
-        Me.Label4.Text = "Tgl. Peminjaman"
+        Me.btnbuku.Location = New System.Drawing.Point(176, 19)
+        Me.btnbuku.Name = "btnbuku"
+        Me.btnbuku.Size = New System.Drawing.Size(32, 20)
+        Me.btnbuku.TabIndex = 6
+        Me.btnbuku.Text = "..."
+        Me.btnbuku.UseVisualStyleBackColor = True
         '
-        'Label5
+        'tbjudulbuku
         '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(359, 22)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(66, 13)
-        Me.Label5.TabIndex = 14
-        Me.Label5.Text = "Kd. Anggota"
+        Me.tbjudulbuku.Location = New System.Drawing.Point(214, 19)
+        Me.tbjudulbuku.Name = "tbjudulbuku"
+        Me.tbjudulbuku.Size = New System.Drawing.Size(272, 20)
+        Me.tbjudulbuku.TabIndex = 7
         '
-        'Label6
+        'tbkdbuku
         '
-        Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(359, 48)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(35, 13)
-        Me.Label6.TabIndex = 15
-        Me.Label6.Text = "Nama"
+        Me.tbkdbuku.Location = New System.Drawing.Point(70, 19)
+        Me.tbkdbuku.Name = "tbkdbuku"
+        Me.tbkdbuku.Size = New System.Drawing.Size(100, 20)
+        Me.tbkdbuku.TabIndex = 6
         '
-        'Label7
+        'dgv
         '
-        Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(359, 74)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(39, 13)
-        Me.Label7.TabIndex = 16
-        Me.Label7.Text = "Alamat"
-        '
-        'Label8
-        '
-        Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(650, 51)
-        Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(66, 13)
-        Me.Label8.TabIndex = 17
-        Me.Label8.Text = "No. Telepon"
-        '
-        'Label9
-        '
-        Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(650, 74)
-        Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(29, 13)
-        Me.Label9.TabIndex = 18
-        Me.Label9.Text = "Role"
-        '
-        'btnanggota
-        '
-        Me.btnanggota.Location = New System.Drawing.Point(594, 19)
-        Me.btnanggota.Name = "btnanggota"
-        Me.btnanggota.Size = New System.Drawing.Size(32, 20)
-        Me.btnanggota.TabIndex = 12
-        Me.btnanggota.Text = "..."
-        Me.btnanggota.UseVisualStyleBackColor = True
+        Me.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv.Location = New System.Drawing.Point(12, 178)
+        Me.dgv.Name = "dgv"
+        Me.dgv.Size = New System.Drawing.Size(860, 218)
+        Me.dgv.TabIndex = 5
         '
         'btnsimpan
         '
--- perpustakaan/perpustakaan/FormPeminjaman.vb
+++ perpustakaan/perpustakaan/FormPeminjaman.vb
@@ -12,7 +12,7 @@ Public Class FormPeminjaman
         FormBuku.ShowDialog()
     End Sub
     Sub isigrid()
-        Dim query As String = "SELECT * FROM tb_keranjang"
+        Dim query As String = "SELECT * FROM tb_keranjang ORDER BY no DESC"
         Dim da As New MySqlDataAdapter(query, konek)
         Dim ds As New DataSet()
         If da.Fill(ds) Then
@@ -22,16 +22,125 @@ Public Class FormPeminjaman
             dgv.DataSource = Nothing
         End If
         If dgv.RowCount > 0 Then
-            'judulgrid()
+            judulgrid()
         End If
     End Sub
+    Sub judulgrid()
+        Dim objAlternatingCellStyle As New DataGridViewCellStyle()
+        dgv.AlternatingRowsDefaultCellStyle = objAlternatingCellStyle
+        Dim style As DataGridViewCellStyle = dgv.Columns(0).DefaultCellStyle
+        dgv.Columns(0).Visible = False
+        dgv.Columns(1).HeaderText = "Kode Buku"
+        dgv.Columns(1).Width = 100
+        dgv.Columns(2).HeaderText = "Judul Buku"
+        dgv.Columns(2).AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        dgv.Columns(3).HeaderText = "Tgl. Pengembalian"
+        dgv.Columns(3).Width = 150
+        objAlternatingCellStyle.BackColor = Color.AliceBlue
+        dgv.SelectionMode = DataGridViewSelectionMode.FullRowSelect
+        dgv.ReadOnly = True
+        dgv.AllowUserToAddRows = False
+    End Sub
     Sub querytambah()
-        Dim q As String = "INSERT INTO tb_keranjang VALUES (@kd_buku, @judul, @tgl_kembali)"
-        Querykeranjang(q, tbkdbuku.Text, tbjudulbuku.Text.ToUpper, dtpkembalibuku.Value)
-        MsgBox("Berhasil tambah data!", MsgBoxStyle.Information, "Informasi")
+        Dim q As String = "INSERT INTO tb_keranjang (kd_buku,judul,tgl_kembali) VALUES (@kd_buku, @judul, @tgl_kembali)"
+        Querykeranjang(q, tbkdbuku.Text, tbjudulbuku.Text.ToUpper, Format(dtpkembalibuku.Value, "yyyy-MM-dd"))
         isigrid()
     End Sub
     Private Sub btninput_Click(sender As Object, e As EventArgs) Handles btninput.Click
         querytambah()
+    End Sub
+    Sub kode()
+        cmd = New MySqlCommand("SELECT kd_peminjaman FROM tb_peminjaman ORDER BY kd_peminjaman DESC LIMIT 1", konek)
+        dr = cmd.ExecuteReader
+        dr.Read()
+        Dim currentdate As DateTime = DateTime.Now
+        Dim kd As String = "PJM" & currentdate.ToString("yyMM")
+        If Not dr.HasRows Then
+            tbkdpeminjaman.Text = kd & "000001"
+        Else
+            Dim hitung As String = Val(Microsoft.VisualBasic.Right(dr.Item("kd_peminjaman").ToString, 6)) + 1
+            If Len(hitung) = 1 Then
+                tbkdpeminjaman.Text = kd & "00000" & hitung
+            ElseIf Len(hitung) = 2 Then
+                tbkdpeminjaman.Text = kd & "0000" & hitung
+            ElseIf Len(hitung) = 3 Then
+                tbkdpeminjaman.Text = kd & "000" & hitung
+            ElseIf Len(hitung) = 4 Then
+                tbkdpeminjaman.Text = kd & "00" & hitung
+            ElseIf Len(hitung) = 5 Then
+                tbkdpeminjaman.Text = kd & "0" & hitung
+            ElseIf Len(hitung) = 6 Then
+                tbkdpeminjaman.Text = kd & hitung
+            End If
+        End If
+        dr.Close()
+    End Sub
+    Sub resetkeranjang()
+        Dim q As String = "TRUNCATE TABLE tb_keranjang"
+        Query(q)
+    End Sub
+    Sub reset()
+        kode()
+        tbkdanggota.Clear()
+        tbnama.Clear()
+        tbalamat.Clear()
+        tbnotelp.Clear()
+        tbrole.Clear()
+        tbkdbuku.Clear()
+        tbjudulbuku.Clear()
+        resetkeranjang()
+        isigrid()
+    End Sub
+
+    Private Sub FormPeminjaman_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        reset()
+    End Sub
+
+    Private Sub btnhpsitem_Click(sender As Object, e As EventArgs) Handles btnhpsitem.Click
+        Try
+            Dim baris As Integer
+            Dim no As Integer
+            Dim kdbrg As String
+            Dim nmbrg As String
+            With dgv
+                baris = .CurrentRow.Index
+                no = .Item(0, baris).Value
+                kdbrg = .Item(1, baris).Value
+                nmbrg = .Item(2, baris).Value
+            End With
+            Dim nhps As Integer
+            nhps = MsgBox("Hapus buku " & nmbrg & " (" & kdbrg & ") dari keranjang?", 48 + 4 + 256, "Konfirmasi")
+            If nhps = 6 Then
+                Dim queryhps As String = "DELETE FROM tb_keranjang WHERE no = " & no
+                Query(queryhps)
+                isigrid()
+            End If
+        Catch ex As Exception
+            MsgBox("Silahkan pilih item buku!", 16, "Perhatian")
+        End Try
+    End Sub
+    Sub simpanpeminjaman()
+        Dim simpandetail As String = "INSERT INTO tb_peminjaman_detail (kd_peminjaman, kd_buku, tgl_kembali) " _
+                                        & "SELECT '" & tbkdpeminjaman.Text & "', kd_buku, tgl_kembali FROM tb_keranjang"
+        Query(simpandetail)
+        Dim simpan As String = "INSERT INTO tb_peminjaman " _
+                            & "VALUES ('" & tbkdpeminjaman.Text & "', '" & Format(dtppinjaman.Value, "yyyy-MM-dd") & "', '" & tbkdanggota.Text & "')"
+        Query(simpan)
+        MsgBox("Peminjaman berhasil!")
+    End Sub
+
+    Private Sub btnsimpan_Click(sender As Object, e As EventArgs) Handles btnsimpan.Click
+        Dim nplh As Integer
+        nplh = MsgBox("Simpan peminjaman?", 48 + 4 + 256, "Konfirmasi")
+        If nplh = 6 Then
+            If tbkdanggota.Text = "" Then
+                MsgBox("Masukkan data anggota!", 16, "Perhatian")
+            ElseIf dgv.RowCount = 0 Then
+                MsgBox("Data buku masih kosong!", 16, "Perhatian")
+            Else
+                simpanpeminjaman()
+                reset()
+            End If
+        End If
     End Sub
 End Class
